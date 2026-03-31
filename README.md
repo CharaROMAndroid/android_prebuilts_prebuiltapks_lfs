29f8008415b68b126e4fa6f9015d176ad6dbc5eb
Chara <bionichuman07@gmail.com>
2026-03-31 10:12:20 -0400

    Add initial set of CharaROM Prebuilt APKs
    These APKs were sourced from https://gitlab.e.foundation/e/os/android_prebuilts_prebuiltapks_lfs. No changes have been done to them.

---
+++ README.md
@@ -0,0 +1,8 @@
+# CharaROM Prebuilt APKs
+
+This is a Git Lfs project. All APKs in this repository are considered as Lfs pointers
+to reduce the size of the whole repository.
+
+## Prerequesites
+
+Please make sure you have **git-lfs** installed on your computer before committing to the project.
---
+++ config/common.mk
@@ -0,0 +1,42 @@
+# Copyright (C) 2025 E FOUNDATION
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/%
+
+# Android Auto Stubs specific for API level > 34
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 34; echo $$?),0)
+PRODUCT_PACKAGES += \
+    AndroidAutoStub
+endif
+
+PRODUCT_PACKAGES += \
+    AccountManager \
+    Ntfy \
+    ParentalControl \
+    Talkback \
+
+
+# eSpeak
+ifeq ($(TARGET_SUPPORTS_32_BIT_APPS),false)
+PRODUCT_PACKAGES += eSpeak
+endif
+
+# Optional applications
+MINIMAL_APPS ?= false
+
+ifeq ($(MINIMAL_APPS),false)
+PRODUCT_PACKAGES += PdfViewer
+PRODUCT_PACKAGES += OpenKeychain
+endif
