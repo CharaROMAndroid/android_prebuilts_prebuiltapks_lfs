1fd1503e0d14789a9b34fa6d6891021ed4500249
Chara <bionichuman07@gmail.com>
2026-08-31 20:40:18 -0400

    Drop AndroidAutoStub, make OKC and Ntfy uninstallable
    - Remove AndroidAutoStub entirely
    - Move Ntfy from system_ext to product partition
    - Move OpenKeychain to product partition
    - Apps in product partition are uninstallable by users

--- config/common.mk
+++ config/common.mk
@@ -29,10 +29,5 @@ MINIMAL_APPS ?= false
 ifeq ($(MINIMAL_APPS),false)
 PRODUCT_PACKAGES += \
     OpenKeychain \
-    Ntfy 
-# Android Auto Stubs specific for API level > 34
-#ifeq ($(shell test $(PLATFORM_SDK_VERSION) -gt 34; echo $$?),0)
-#PRODUCT_PACKAGES += \
-#    AndroidAutoStub
-#endif
+    Ntfy
 endif
